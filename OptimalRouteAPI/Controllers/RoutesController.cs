5f3e40d55a4c12695c955130dbfa9c4ceb536030
nmedinaj <nmedina@academianous.es>
2025-02-12 21:27:48 +0100

    feat: project configuration and dummy endpoint

---
+++ OptimalRouteAPI/Controllers/RoutesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OptimalRouteAPI.Models;
+
+namespace OptimalRouteAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class RoutesController : ControllerBase
+	{
+		private readonly ILogger<RoutesController> _logger;
+		public RoutesController(ILogger<RoutesController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet(Name = "route")]
+		public Models.Route Get()
+		{
+			return new Models.Route()
+			{
+				RouteName = "Route",
+				StartRoute = "New York",
+				EndRoute = "Boston"
+			};
+		}
+	}
+}
---
+++ OptimalRouteAPI/Models/Route.cs
@@ -0,0 +1,9 @@
+﻿namespace OptimalRouteAPI.Models
+{
+	public class Route
+	{
+		public string RouteName { get; set; }
+		public string StartRoute { get; set; }
+		public string EndRoute { get; set; }
+	}
+}
